0407754c43a752b094bc93de34774926a4028b7a
Jessica Newman <jess.newman19@gmail.com>
2022-02-02 22:22:55 -0500

    commit tree f1fbf7548bba07889a004a511ccba5691d80435a

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+console.log(shout("hello"));
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+console.log(whisper("hello"));
+
+function logShout(string) { 
+    console.log(string.toUpperCase());
+}
+
+logShout("hello");
+
+function logWhisper(string) { 
+    console.log(string.toLowerCase());
+}
+
+logWhisper("hello");
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) { 
+        return "I can't hear you!";
+    }
+    else if (string === string.toUpperCase()) { 
+        return "YES INDEED!";
+    }
+    else if (string === "Let's have dinner together!") { 
+        return "I would love to!";
+    }
+    }
+
+    console.log(sayHiToHeadphonedRoommate("hello"));
+    console.log(sayHiToHeadphonedRoommate("HELLO")); 
+    console.log(sayHiToHeadphonedRoommate("Let's have dinner together"));
